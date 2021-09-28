b9a3fa539f7e4f0ae962c2c7983a18f110450084
jabed366 <jabedhasan231@gmail.com>
2021-09-28 19:00:30 +0600

    First Commit

---
+++ arrayAdd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a[4],i,sum;
+    sum=0;
+    a[0]=50;
+    a[1]=20;
+    a[2]=30;
+    a[3]=40;
+    for(i=0;i<=3;i++){
+        printf("%d\n",a[i]);
+        sum=sum+a[i];
+
+    }
+    printf("%d\n",sum);
+    return 0;    
+}
