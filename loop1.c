76a230aa095b8072c2c90c5234025e416694e500
jabed366 <jabedhasan231@gmail.com>
2021-10-02 15:53:48 +0600

    First Commit

---
+++ loop1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int n,i;
+    int num[]={1,2,3,4,5,6,7,8,9,10};
+    for(n=1,i=0;n<=10,i<10;n++,i++){
+    printf("the number: %d %d\n",n,num[i]);
+    }
+    return 0;
+}
