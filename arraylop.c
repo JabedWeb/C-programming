92d2ea9669ca89da8738c5e4fd6ae69ca3a17323
jabed366 <jabedhasan231@gmail.com>
2021-10-06 03:01:27 +0600

    First Commit

--- arraylop.c
+++ arraylop.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 int main(){
-    int n;
-    int i[5];
-    printf("the numer ");
-    for(n=0; n<6;n++){
-    scanf("%d\n",&i[n]);
+    int i[5],n;
+    for(n=0; n<=4;n++){
+    scanf("%d",&i[n]);
     }
-     for(n=0; n<6;n++){
-    printf(" the number is %d\n",i[n]);
+    for(n=0; n<=4;n++){
+    printf("the number is %d\n",i[n]);
     }
    return 0;
 }
