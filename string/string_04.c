9b5eadd01ca04ed8f3debe0257a636bd33299c0c
jabed366 <jabedhasan231@gmail.com>
2021-10-17 00:52:25 +0600

    First Commit

--- string/string_04.c
+++ string/string_04.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
+int total_lenth(char country[]){
+  int i;
+  int lenth=0;
+  for(i=0;i<country[i]!='\0';i++){
+    lenth++;
+  }
+  return lenth;
+}
 int main(){
-    int i,lenth=100;
-  char country[100]={'B','a','n','G','l','a','d','e','s','h'};
-  printf("%s\n",country);
-  //A=65
-  //a=97
-// a to A = A + latter-a= 65+97-97
-//A TO a=65+32 B to b=
-
-
-
-   for(i=0;i<lenth;i++){
-       if(country[i]>=65 && country[i]<=90){
-        //    country[i]=country[i]+32;
-           country[i]='a'+(country[i]-'A');
-       }
-   }
-   printf("%s",country);
-   return 0;
-
-   //input BanGldesh
-   //output bangladesh
+    int i,lenth;
+  char country[100];
+    scanf("%s",country);
+    int result=total_lenth(country);
+    printf("%d",result);
 }
